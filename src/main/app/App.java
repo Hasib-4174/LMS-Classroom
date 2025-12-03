df7cef747ec06f47f33a5bd8f38da23112708c12
Hasib-4174 <hasibkhan4174@gmail.com>
2025-12-03 23:39:45 +0600

    add README.md & Class test: Student, Course

--- src/main/app/App.java
+++ src/main/app/App.java
@@ -1,10 +1,12 @@
 package app;
-import utils.*;
+
+import users.Student;
 
 public class App {
     public static void main(String[] args) {
-        System.out.println("hello world");
-        int sum = Add.add(1, 2, 3, 4, 5);
-        System.out.println("Sum: " + sum);
+        Student s = new Student("Shamim", "2521228042", "shamim252@northsouth.edu", "shamim252");
+        s.enrollCourse("java-OOP", "CSE215", "SAM3", "RA 8:00-9:30");
+        s.enrollCourse("Python-OOP", "CSE216", "Nova", "MW 9:40-11:10");
+        s.showInfo();
     }
 }
